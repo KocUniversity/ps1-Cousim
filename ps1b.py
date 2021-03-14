9e446d6a9f89e76e4d78beb64d4e8005b53395aa
Cousim <kguler20@ku.edu.tr>
2021-03-14 17:34:14 +0000

    PS1 14.03.21 2H

--- ps1b.py
+++ ps1b.py
@@ -2,8 +2,10 @@ n, B = list(map(int, input().strip().split()))
 T = 0
 
 # your code here
+
 T = 1
 formuladivT = 0
+
 for i in range(1, n+1):
   if (i) % 2 == 0:
     pint = 2**(i) + 1
@@ -11,13 +13,23 @@ for i in range(1, n+1):
     pint = 3**(i) + 1 
   formuladivT += pint**(n - i)
   
-while formuladivT * T < B:
-  T += 1
+high = 10000
+low = 1
+Trial = (high + low)/2.0
 
-if T > 10000:
-  T = -1
+while formuladivT * Trial != B:
+  if formuladivT * Trial < B:
+    low = Trial
+  else:
+    high = Trial
+  Trial = (high + low)/2.0
+
+if formuladivT * int(Trial) < B:
+  Trial = int(Trial) + 1
 else:
   pass
+
+T = Trial
 
 # please do not modify the input and print statements
 # and make sure that your code does not have any other print statements
